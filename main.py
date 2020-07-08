4bec0eb70448f6513811cf8cd71bc127dccc0cf7
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-07-08 17:35:10 -0300

    Add method to generate postman collections

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from openapi.models import OpenApiSpecification
 
 app = FastAPI()
 
-@app.get('/health')
+
+@app.get('/api/v1/health')
 def health_check():
     return {'ok': True, 'message': 'OpenAPI2Postman is up and running!'}
+
+
+@app.post('/api/v1/postman-collection')
+def generate_postman_collection(openapi: OpenApiSpecification):
+    return {'title': openapi.info.title}
