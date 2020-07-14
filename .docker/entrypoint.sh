a675662588b2e148abe6df01de31c37770844a26
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-07-14 20:02:05 -0300

    Add docker entrypoint sh

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 cd ./app
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+
+if [[ -z "${ENV}" ]]; then
+  export GUNICORN_CMD_ARGS="-w 4 -k uvicorn.workers.UvicornWorker"
+else
+  export GUNICORN_CMD_ARGS="--bind=0.0.0.0 -w 4 -k uvicorn.workers.UvicornWorker"
+fi
+
+gunicorn main:app
