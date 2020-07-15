3e002e93af58ddbd837bf749c292a6dcb21ecb38
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-07-15 11:24:10 -0300

    Remove unused scripts

--- .docker/run-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-container_name="openapi-to-pm"
-docker exec ${container_name} /bin/bash -c "cd /usr/src/app && pytest"
--- .docker/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-container_name="openapi-to-pm"
-image_name="mellomaths/openapi-to-pm"
-docker build -t ${image_name} .
-docker run -d --publish 80:8000 \
-  --name=${container_name} \
-  -e ENV='development' \
-  ${image_name}
