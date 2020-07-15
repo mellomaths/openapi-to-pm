ca08d4b3bd4bfba4f5aac46ac4c52904df93daa6
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-07-15 10:39:28 -0300

    Change docker entrypoint for runnig outside app directory

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cd ./app
-
 if [[ -z "${ENV}" ]]; then
   export GUNICORN_CMD_ARGS="-w 4 -k uvicorn.workers.UvicornWorker"
 else
   export GUNICORN_CMD_ARGS="--bind=0.0.0.0 -w 4 -k uvicorn.workers.UvicornWorker"
 fi
 
-gunicorn main:app
+gunicorn app.main:app
