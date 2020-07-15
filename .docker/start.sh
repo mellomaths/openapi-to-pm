5a3f4b4aa5c69c01fcc61a929246fef8ce03b97a
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-07-15 10:59:43 -0300

    Update start.sh script to run in development mode

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-app="mellomaths/openapi-to-pm"
-docker build -t ${app} .
-docker run -d --publish 8000:8000 \
-  --name=${app} \
-  -v $PWD:/app ${app}
+container_name="openapi-to-pm"
+image_name="mellomaths/openapi-to-pm"
+docker build -t ${image_name} .
+docker run -d --publish 80:8000 \
+  --name=${container_name} \
+  -e ENV='development' \
+  ${image_name}
